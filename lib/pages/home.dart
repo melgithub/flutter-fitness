70113141dfc7c2897fd91c826d5cd10a90cac849
Melissa Koven <melissa@Melissas-Mac-Studio-2.local>
2024-03-04 18:55:49 -0700

    More search bar edits and refactoring

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,44 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: appBar(),
-        body: Column(children: [
-          // Padding(
-          //   padding: EdgeInsets.all(12),
-          // ),
-          Container(
-            margin: const EdgeInsets.only(top: 20, left: 20, right: 20),
-            decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.18),
-                  spreadRadius: 5,
-                  blurRadius: 7,
-                  offset: const Offset(0, 3), // changes position of shadow
-                )
-              ],
+    return Scaffold(appBar: appBar(), body: Column(children: [_searchField()]));
+  }
+
+  Container _searchField() {
+    return Container(
+        margin: const EdgeInsets.only(top: 20, left: 20, right: 20),
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.18),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: const Offset(0, 3), // changes position of shadow
+            )
+          ],
+        ),
+        child: TextField(
+          decoration: InputDecoration(
+            prefixIcon: const Icon(CupertinoIcons.bubble_right),
+            filled: true,
+            fillColor: Colors.white,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide.none, // Remove the border side
             ),
-            child: TextField(
-              decoration: InputDecoration(
-                prefixIcon: const Icon(CupertinoIcons.bubble_right),
-                filled: true,
-                fillColor: Colors.white,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide.none, // Remove the border side
+            hintText: 'Ask me something!',
+            // labelText: 'Curious?',
+            // suffixIcon: const Icon(CupertinoIcons.barcode),
+            suffixIcon: Container(
+              width:
+                  100, // we had to do this so the text appears and isn't overlapped by the icon
+              child: const IntrinsicHeight(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    VerticalDivider(
+                      color: Colors.grey,
+                      thickness: 1,
+                      width: 1,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(14),
+                      child: Icon(CupertinoIcons.barcode),
+                    )
+                  ],
                 ),
-                // labelText: 'Curious?',
-                hintText: 'Ask me anything',
               ),
             ),
-          )
-        ]));
+          ),
+        ));
   }
 
   AppBar appBar() {
